6b9a4acc21681826210256642d9fda4c797b875a
robert <robert@s7on.de>
2024-12-07 14:51:10 +0100

    game-object updated

--- game_object.gd
+++ game_object.gd
@@ -1,6 +1,15 @@
-extends CharacterBody2D
+class_name GameObject extends CharacterBody2D
 
-class_name GameObject
+const ObjectTypeResource = preload("res://shared/object_type.gd")
+const GamePhaseResource = preload("res://shared/game_phase.gd")
 
 var id: int = 0
-var label: String = "undefined"
+@export var label: String
+@export var type: ObjectTypeResource.ObjectType
+@export var phase: GamePhaseResource.Phase
+
+func get_phase() -> GamePhaseResource.Phase:
+	return phase
+
+func get_type() -> ObjectTypeResource.ObjectType:
+	return type

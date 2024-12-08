9ca652a58b53706da6324627952d370c69d5bd71
robert <robert@s7on.de>
2024-12-08 20:25:29 +0100

    wss added to ip

--- connection_constants.gd
+++ connection_constants.gd
@@ -4,4 +4,4 @@ class_name ConnectionConstants
 
 const PORT: int = 15000
 const MAX_CONNECTIONS: int = 10
-const SERVER_IP: String = "static.110.5.69.159.clients.your-server.de"
+const SERVER_IP: String = "wss://static.110.5.69.159.clients.your-server.de"

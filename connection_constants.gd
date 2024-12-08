c4c7aef00439978960c17dbc958f9aecf475e6ba
robert <robert@s7on.de>
2024-12-08 15:37:29 +0100

    constant works now

--- connection_constants.gd
+++ connection_constants.gd
@@ -4,4 +4,4 @@ class_name ConnectionConstants
 
 const PORT: int = 15000
 const MAX_CONNECTIONS: int = 10
-const SERVER_IP: String = "2a01:4f8:c2c:61cd::1"
+const SERVER_IP: String = "[2a01:4f8:c2c:61cd::1]"
